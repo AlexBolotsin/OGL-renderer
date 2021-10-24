02909693363035aea8a3a9a925d3c0a8b3151e0c
Alex Bolotsin <alexbolotsin@gmail.com>
2021-10-24 23:42:28 +0300

    Fixed dimming of the whole scene

--- ShadowMap.cpp
+++ ShadowMap.cpp
@@ -26,6 +26,8 @@ bool ShadowMap::Init(GLuint width, GLuint height)
     glBindTexture(GL_TEXTURE_2D, shadowMap);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH_COMPONENT, shadowWidth, shadowHeight, 0, GL_DEPTH_COMPONENT, GL_FLOAT, nullptr);
     
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameterf(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
     float border[] = { 1.0f, 1.0f, 1.0f, 1.0f };
